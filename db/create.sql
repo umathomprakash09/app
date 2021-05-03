37e179d9aa5d9426523635aee0473c18c8c45262
Om <omprakashumath2203@gmail.com>
2021-05-03 14:51:36 +0530

    Database add

---
+++ db/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employees (
+    id int  NOT NULL AUTO_INCREMENT PRIMARY key,
+    name varchar(50) NOT NULL,
+    position varchar(50),
+    office varchar(50),
+    age int,
+    start_date date,
+    salary float
+);
